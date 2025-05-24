f8cf7ae91850dcbdf1314bdfa25a522d66be8238
helenakristela <hkristela03@gmail.com>
2025-05-24 08:52:56 +0700

    fix: memperbaiki showbag

--- src/modules/bag.pl
+++ src/modules/bag.pl
@@ -1,5 +1,6 @@
-:- module(bag, [setBag/0, showBag/0, is_slot_empty/1]).
+:- module(bag, [setBag/0, showBag/0, is_slot_empty/1, add_item_to_bag/1]).
 :- dynamic bag/2.
+:- use_module(items).
 
 setBag :-
     retractall(bag(_, _)),
@@ -9,17 +10,43 @@ setBag :-
 initBag(Start, End, _) :- Start > End, !.
 initBag(Start, End, Value) :-
     assertz(bag(Start, Value)),
-    NewStart is Start + 1,
-    initBag(NewStart, End, Value).
+    Next is Start + 1,
+    initBag(Next, End, Value).
 
 showBag :-
     write('=== Isi Tas ==='), nl,
     write('Slot 00-19: Pokeball'), nl,
-    write('Slot 20-39: Kosong'), nl,
-    write('Detail:'), nl,
+    write('Slot 20-39: Item/Kosong'), nl, nl,
     forall(between(0, 39, Index),
         (bag(Index, Item),
-        format('Slot ~|~`0t~d~2+: ~w~n', [Index, Item]))).
+        format_item(Index, Item))).
+
+format_item(Index, empty) :-
+    format('Slot ~|~`0t~d~2+: [Kosong]~n', [Index]).
+format_item(Index, pokeball(empty)) :-
+    format('Slot ~|~`0t~d~2+: Pokeball (Kosong)~n', [Index]).
+format_item(Index, pokeball(filled(_))) :-
+    format('Slot ~|~`0t~d~2+: Pokeball (Terisi)~n', [Index]).
+format_item(Index, Item) :-
+    (current_predicate(item/3), item(Item, Type, _)) -> 
+        format('Slot ~|~`0t~d~2+: ~w (~w)~n', [Index, Item, Type])
+    ;
+        format('Slot ~|~`0t~d~2+: ~w (Item)~n', [Index, Item]).
 
 is_slot_empty(Index) :-
+    between(0, 39, Index),
     bag(Index, empty).
+
+add_item_to_bag(Item) :-
+    find_empty_slot(Index),
+    retract(bag(Index, empty)),
+    assertz(bag(Index, Item)),
+    format('Item ~w ditambahkan ke slot ~w~n', [Item, Index]).
+
+find_empty_slot(Index) :-
+    between(20, 39, Index),
+    bag(Index, empty),
+    !.
+find_empty_slot(_) :-
+    write('Tas penuh! Tidak ada slot kosong.'), nl,
+    fail.
